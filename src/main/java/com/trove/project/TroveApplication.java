5cb8a6dcac3fc560fe9b15d0f09c1797e32d567b
joyce-adelle <jadelusi11@gmail.com>
2021-09-02 17:13:58 +0100

    init commit

---
+++ src/main/java/com/trove/project/TroveApplication.java
@@ -0,0 +1,13 @@
+package com.trove.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TroveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trove/project/TroveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trove.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TroveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
